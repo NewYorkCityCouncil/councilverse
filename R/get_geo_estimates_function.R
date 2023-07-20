1d1b52b17adaa8176955ae9368bbc27a11a563a1
Nicholas Montalbano <65362910+nmontalbanocouncil@users.noreply.github.com>
2023-07-20 15:23:35 -0400

    try#2 for get_geo_estimates: made edits requested by cz

--- R/get_geo_estimates_function.R
+++ R/get_geo_estimates_function.R
@@ -1,21 +1,24 @@
 #' Function to retrieve acs estimates from csvs at different geographies
 #'
 #'
-#' @param name Name of the csv. If NULL, returns a list of possible geography csvs.
+#' @param geo Name of the geography with associated csv in "extdata". If NULL, returns a list of possible geography csvs.
 #' @import dplyr
-#' @return sf for the specified geography, or list of geographies if none is specified
+#' @return sf for the specified geography, or message with list of geographies if none is specified
 #' @export
 #'
 
-get_geo_estimates <- function(name = NULL) {
+get_geo_estimates <- function(geo = NULL) {
 
   csv_names <- dir(system.file("extdata", package = "councilverse"))
   geo_csv_names <- csv_names[grepl("geographies", csv_names)]
+  geo_names <- stringr::str_extract(geo_csv_names, "[^-]+")
 
-  if (is.null(name)) {
-    geo_csv_names
+  if (is.null(geo)) {
+    message("get_geo_estimates() requires a 'geo' parameter.", "\n",
+            "Please choose from the following:\n",
+            paste0('"',geo_names, '"', collapse = "\n"))
   } else {
-    readr::read_csv(fs::path_package("extdata", name, package = "councilverse")) %>%
+    readr::read_csv(fs::path_package("extdata", glue::glue("{geo}-geographies.csv"), package = "councilverse")) %>%
       janitor::clean_names() %>%
       sf::st_as_sf(wkt = "geometry", crs = 4326) %>%
       st_transform("+proj=longlat +datum=WGS84")
