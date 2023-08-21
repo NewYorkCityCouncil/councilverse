457a2a897af7198d4f71c6c8cb9262bfc3b5bca1
Nicholas Montalbano <65362910+nmontalbanocouncil@users.noreply.github.com>
2023-08-21 13:12:59 -0400

    add get_census_variables() and census_demo_variables to replace python view_variables()

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Census Demographic Variables
+#'
+#' @format ## `census_demo_variables`
+#' A data frame with 68 rows and 2 columns:
+#' \describe{
+#'   \item{var_code}{Census Variable Code}
+#'   \item{var_name}{Census Variable Name}
+#' }
+"census_demo_variables"
---
+++ R/get_census_variables_function.R
@@ -0,0 +1,11 @@
+#' Function to retrieve the available census demographic variables
+#'
+#'
+#' @return data frame of available variables
+#' @export
+#'
+
+# simply access census_demo_variables, can add functionality in the future if necessary
+get_census_variables <- function() {
+  councilverse::census_demo_variables
+  }
