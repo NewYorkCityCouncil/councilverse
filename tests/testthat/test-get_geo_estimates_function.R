b93e31aa226f3199b9ef7c7c8d859591c4732e2c
Nicholas Montalbano <65362910+nmontalbanocouncil@users.noreply.github.com>
2023-12-13 16:34:03 -0500

    grouping tests together

--- tests/testthat/test-get_geo_estimates_function.R
+++ tests/testthat/test-get_geo_estimates_function.R
@@ -1,7 +1,6 @@
-test_that("warnings and output are approriate", {
+test_that("missing and incorrect geo inputs are handled appropriately", {
 
   ### EXPECT MESSAGE
-  ## geo messages
   # no geo w/ no year
   expect_message(get_geo_estimates(), "requires a 'geo' parameter", class = "simpleMessage")
   # no geo w/ year
@@ -9,15 +8,23 @@ test_that("warnings and output are approriate", {
   # invalid geo
   expect_message(get_geo_estimates("block party", "2023"), "This geography could not be found", class = "simpleMessage")
 
-  ## boundary_year messages
+})
+
+test_that("missing, unnecessary, and incorrect boundary_year inputs are handled appropriately", {
+
+  ### EXPECT MESSAGE
   # council w/ no year
   expect_message(get_geo_estimates("council"), "Setting `boundary_year`", class = "simpleMessage")
   # council w/ incorrect year
   expect_message(get_geo_estimates("council", "2053"), "`boundary_year` must be '2013' or '2023'", class = "simpleMessage")
-  # non-council with year
+  # non-council with unnecessary year
   expect_message(get_geo_estimates("borough", "2023"), "`boundary_year` is only relevant for `geo = council`", class = "simpleMessage")
   # non-council with incorrect year
   expect_message(get_geo_estimates("borough", "2043"), "`boundary_year` is only relevant for `geo = council`", class = "simpleMessage")
+
+})
+
+test_that("correct geo and boundary_year inputs work", {
 
   ### EXPECT NO MESSAGE
   # council 2023
@@ -26,6 +33,5 @@ test_that("warnings and output are approriate", {
   expect_no_message(get_geo_estimates("council", "2013"), message = "`boundary_year` must be '2013' or '2023'", class = "simpleMessage")
   # non-council w/ no year
   expect_no_message(get_geo_estimates("borough"), message = "`boundary_year` must be '2013' or '2023'", class = "simpleMessage")
-
 
 })
