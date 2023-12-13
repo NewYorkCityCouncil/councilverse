9780d123fde32d1732084aa62eda0341b1ba65c2
Nicholas Montalbano <65362910+nmontalbanocouncil@users.noreply.github.com>
2023-12-13 16:15:57 -0500

    fix get_geo_estimates_function.R and add tests/testthat

--- R/get_geo_estimates_function.R
+++ R/get_geo_estimates_function.R
@@ -10,22 +10,24 @@
 
 get_geo_estimates <- function(geo = NULL, boundary_year = NULL) {
 
+  # locate available csv files
   csv_names <- dir(system.file("extdata", package = "councilverse"))
   geo_csv_names <- csv_names[grepl("geographies", csv_names)]
   geo_names <- stringr::str_extract(geo_csv_names, "[^-]+")
 
   # use boundary year information to collect the correct csv later
   boundary_year_num <- stringr::str_sub(boundary_year, -2)
-  boundary_year_ext <- dplyr::case_when(
-    # ignore the boundary year if council is not the geo
-    !is.null(boundary_year) & geo != "council" ~ "",
-    !is.null(boundary_year) & (boundary_year != "2013" | boundary_year != "2023") & geo == "council" ~ "_b13",
-    !is.null(boundary_year) & geo == "council" ~ glue::glue("_b{boundary_year_num}"),
-    # default to council 2013 boundaries if council is geo and no boundary year is provided
-    is.null(boundary_year) & geo == "council" ~ "_b13",
-    TRUE ~ ""
-  )
 
+  # function to read and wrangles geo csv's
+  # note: only year used so far is 2021 so all files end in _2021
+  read_geos <- function(geo = NULL, boundary_year_ext = NULL) {
+    readr::read_csv(fs::path_package("extdata", glue::glue("{geo}-geographies{boundary_year_ext}_2021.csv"), package = "councilverse")) %>%
+    janitor::clean_names() %>%
+    sf::st_as_sf(wkt = "geometry", crs = 4326) %>%
+    sf::st_transform("+proj=longlat +datum=WGS84")
+  }
+
+  # different input cases. can check in tests/testthat/test-get_geo_estimates_function.R
   if (is.null(geo)) {
     message("get_geo_estimates() requires a 'geo' parameter.", "\n",
             "Please choose from the following:\n",
@@ -34,13 +36,19 @@ get_geo_estimates <- function(geo = NULL, boundary_year = NULL) {
     message("This geography could not be found", "\n",
             "Please choose from the following:\n",
             paste0('"',geo_names, '"', collapse = "\n"))
-  } else {
-    if(!is.null(boundary_year) & geo != "council") message("`boundary_year` is only relevant for `geo = council`. Overriding boundary_year input to NULL")
-    if(boundary_year != "2013" & boundary_year != "2023") message("`boundary_year` must be '2013' or '2023'. Overriding boundary_year input to 2013 if `geo = council` or NULL if `geo != council`.")
-    # only year used so far is 2021
-    readr::read_csv(fs::path_package("extdata", glue::glue("{geo}-geographies{boundary_year_ext}_2021.csv"), package = "councilverse")) %>%
-      janitor::clean_names() %>%
-      sf::st_as_sf(wkt = "geometry", crs = 4326) %>%
-      sf::st_transform("+proj=longlat +datum=WGS84")
+  } else if (is.null(boundary_year) & geo == "council"){
+    message("Setting `boundary_year` as 2013. `boundary_year` can be '2013' or '2023'.")
+    read_geos(geo, "_b13")
+  } else if (is.null(boundary_year) & geo != "council"){
+    read_geos(geo, "")
+  } else if (geo != "council"){
+    message("`boundary_year` is only relevant for `geo = council`. Overriding boundary_year input to NULL")
+    read_geos(geo, "")
+  } else if ((boundary_year != "2013" & boundary_year != "2023") & geo == "council") {
+    message("`boundary_year` must be '2013' or '2023'. Overriding boundary_year input to 2013.")
+    read_geos(geo, "_b13")
+  } else if (geo == "council"){
+    read_geos(geo, glue::glue("_b{boundary_year_num}"))
   }
 }
+
