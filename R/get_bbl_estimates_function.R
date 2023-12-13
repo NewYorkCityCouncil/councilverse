9605fc3ba93a56785b157974e0c06452cb1443f3
Nicholas Montalbano <65362910+nmontalbanocouncil@users.noreply.github.com>
2023-12-13 09:51:05 -0500

    add updates to csv extensions for get_*_estimates_function.R

--- R/get_bbl_estimates_function.R
+++ R/get_bbl_estimates_function.R
@@ -6,6 +6,6 @@
 #'
 
 get_bbl_estimates <- function() {
-  readr::read_csv(fs::path_package("extdata", glue::glue("bbl-population-estimates.csv"), package = "councilverse")) %>%
+  readr::read_csv(fs::path_package("extdata", glue::glue("bbl-population-estimates_2021.csv"), package = "councilverse")) %>%
     janitor::clean_names()
   }
--- R/get_geo_estimates_function.R
+++ R/get_geo_estimates_function.R
@@ -1,13 +1,14 @@
 #' Function to retrieve acs estimates from csvs at different geographies
 #'
 #'
-#' @param geo Name of the geography with associated csv in "extdata". If NULL, returns a list of possible geography csvs. Options: "borough", "community", "council", "neighborhood", "precinct", "schooldist".
+#' @param geo (string). Name of the geography with associated csv in "extdata". If NULL, returns a list of possible geography csvs. Options: "borough", "community", "council", "neighborhood", "precinct", "schooldist".
+#' @param boundary_year (string). Year for the geographic boundary (i.e. geo). Currently only relevant for council districts, which have the options "2013" and "2023".
 #' @import dplyr
 #' @return sf for the specified geography, or message with list of geographies if none is specified
 #' @export
 #'
 
-get_geo_estimates <- function(geo = NULL) {
+get_geo_estimates <- function(geo = NULL, boundary_year = NULL) {
 
   csv_names <- dir(system.file("extdata", package = "councilverse"))
   geo_csv_names <- csv_names[grepl("geographies", csv_names)]
@@ -23,7 +24,10 @@ get_geo_estimates <- function(geo = NULL) {
             paste0('"',geo_names, '"', collapse = "\n"))
   }
   else {
-    readr::read_csv(fs::path_package("extdata", glue::glue("{geo}-geographies.csv"), package = "councilverse")) %>%
+    boundary_year_cut <- stringr::str_sub(boundary_year, -2)
+    boundary_year_ext <- ifelse(is.null(boundary_year), "", glue::glue("_b{boundary_year_cut}"))
+    # only year used so far is 2021
+    readr::read_csv(fs::path_package("extdata", glue::glue("{geo}-geographies{boundary_year_ext}_2021.csv"), package = "councilverse")) %>%
       janitor::clean_names() %>%
       sf::st_as_sf(wkt = "geometry", crs = 4326) %>%
       sf::st_transform("+proj=longlat +datum=WGS84")
