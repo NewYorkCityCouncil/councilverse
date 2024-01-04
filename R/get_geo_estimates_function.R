0f1aae73f5efbbd24bc57cbac8089e81e2fe59fc
Rachel Avram <75150847+ravram193@users.noreply.github.com>
2024-01-04 10:10:05 -0500

    Add files via upload

--- R/get_geo_estimates_function.R
+++ R/get_geo_estimates_function.R
@@ -8,7 +8,51 @@
 #' @export
 #'
 
-get_geo_estimates <- function(var_codes = NULL, geo = NULL, boundary_year = NULL) {
+get_geo_estimates <- function(geo = NULL, boundary_year = NULL) {
+  
+  # locate available csv files
+  csv_names <- dir(system.file("extdata", package = "councilverse"))
+  geo_csv_names <- csv_names[grepl("geographies", csv_names)]
+  geo_names <- stringr::str_extract(geo_csv_names, "[^-]+") %>% unique()
+  
+  # use boundary year information to collect the correct csv later
+  boundary_year_num <- stringr::str_sub(boundary_year, -2)
+  
+  # function to read and wrangles geo csv's
+  # note: only year used so far is 2021 so all files end in _2021
+  read_geos <- function(geo = NULL, boundary_year_ext = NULL) {
+    readr::read_csv(fs::path_package("extdata", glue::glue("{geo}-geographies{boundary_year_ext}_2021.csv"), package = "councilverse")) %>%
+      janitor::clean_names() %>%
+      sf::st_as_sf(wkt = "geometry", crs = 4326) %>%
+      sf::st_transform("+proj=longlat +datum=WGS84")
+  }
+  
+  # different input cases. can check in tests/testthat/test-get_geo_estimates_function.R
+  if (is.null(geo)) {
+    message("get_geo_estimates() requires a 'geo' parameter.", "\n",
+            "Please choose from the following:\n",
+            paste0('"',geo_names, '"', collapse = "\n"))
+  } else if (!(geo %in% geo_names)) {
+    message("This geography could not be found", "\n",
+            "Please choose from the following:\n",
+            paste0('"',geo_names, '"', collapse = "\n"))
+  } else if (is.null(boundary_year) & geo == "council"){
+    message("Setting `boundary_year` as 2013. `boundary_year` can be '2013' or '2023'.")
+    read_geos(geo, "_b13")
+  } else if (is.null(boundary_year) & geo != "council"){
+    read_geos(geo, "")
+  } else if (geo != "council"){
+    message("`boundary_year` is only relevant for `geo = council`. Overriding boundary_year input to NULL")
+    read_geos(geo, "")
+  } else if ((boundary_year != "2013" & boundary_year != "2023") & geo == "council") {
+    message("`boundary_year` must be '2013' or '2023'. Overriding boundary_year input to 2013.")
+    read_geos(geo, "_b13")
+  } else if (geo == "council"){
+    read_geos(geo, glue::glue("_b{boundary_year_num}"))
+  }
+}
+
+test_get_geo_estimates <- function(var_codes = NULL, geo = NULL, boundary_year = NULL) {
   
   # locate available csv files
   csv_names <- dir(system.file("extdata", package = "councilverse"))
@@ -60,17 +104,17 @@ get_geo_estimates <- function(var_codes = NULL, geo = NULL, boundary_year = NULL
       # creating list of desired variables names (for sub-setting final df)
       #col_names <- c(paste(geo_name_convert[geo], add_year, sep=''), 'geometry')
       col_names <- c(geo, add_year, sep=''), 'geometry')
-for(i in 1:nrow(demo_variables)) {
-  row <- demo_variables[i,]
-  col_names <- append(col_names, row$cleaned_name) # adding percent version
-  col_names <- append(col_names, str_sub(row$cleaned_name, 9)) # adding number   version
-  col_names <- append(col_names, paste(row$cleaned_name, 'moe', sep = '_')) # adding % MOE
-  col_names <- append(col_names, paste(str_sub(row$cleaned_name, 9), 'moe', sep = '_')) # adding number MOE
-  col_names <- append(col_names, paste(str_sub(row$cleaned_name, 9), 'cv', sep = '_')) # adding CV
-}
-return(geo_df[,col_names])
+      for(i in 1:nrow(demo_variables)) {
+        row <- demo_variables[i,]
+        col_names <- append(col_names, row$cleaned_name) # adding percent version
+        col_names <- append(col_names, str_sub(row$cleaned_name, 9)) # adding number   version
+        col_names <- append(col_names, paste(row$cleaned_name, 'moe', sep = '_')) # adding % MOE
+        col_names <- append(col_names, paste(str_sub(row$cleaned_name, 9), 'moe', sep = '_')) # adding number MOE
+        col_names <- append(col_names, paste(str_sub(row$cleaned_name, 9), 'cv', sep = '_')) # adding CV
+        }
+      return(geo_df[,col_names])
+      }
     }
-  }
   
   # different input cases. can check in tests/testthat/test-get_geo_estimates_function.R
   
