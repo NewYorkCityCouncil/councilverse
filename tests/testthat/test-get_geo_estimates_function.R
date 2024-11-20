cb554c3a78037914939d84f4412215e28bbf4a9b
Rachel Avram <75150847+ravram193@users.noreply.github.com>
2024-11-20 16:19:59 -0500

    Incorporationg councilcount updates

--- tests/testthat/test-get_geo_estimates_function.R
+++ tests/testthat/test-get_geo_estimates_function.R
@@ -1,60 +1,64 @@
 test_that("missing parameters are correctly handled", {
 
   ### EXPECT MESSAGE
-  # default var_codes, no geo, no boundary_year
-  expect_message(get_geo_estimates(), "requires a 'geo' parameter", class = "simpleMessage")
-  # default var_codes, no geo, yes boundary_year
-  expect_message(get_geo_estimates(boundary_year = "2023"), "requires a 'geo' parameter", class = "simpleMessage")
-  # yes var_codes, no geo, no boundary_year,
-  expect_message(get_geo_estimates(var_codes = c("DP02_0113E","DP02_0115E")), "requires a 'geo' parameter", class = "simpleMessage")
-  # yes var_codes, no geo, yes boundary_year
-  expect_message(get_geo_estimates(var_codes = c("DP02_0113E","DP02_0115E"), boundary_year = "2023"), "requires a 'geo' parameter", class = "simpleMessage")
-  # geo and boundary_year provided without parameter names
-  expect_message(get_geo_estimates("councildist", "2023"), "boundary_year input used for var_codes parameter", class = "simpleMessage")
+  # no inputs
+  expect_message(get_geo_estimates(), "requires an `acs_year` parameter", class = "simpleMessage")
+  # no `acs_year`, default `var_codes`, yes `geo`, no `boundary_year`
+  expect_message(get_geo_estimates(geo="borough"), "requires an `acs_year` parameter", class = "simpleMessage")
+  # yes `acs_year`, default `var_codes`, no `geo`, no `boundary_year`
+  expect_message(get_geo_estimates(acs_year="2022"), "requires a `geo` parameter", class = "simpleMessage")
+  # no `acs_year`, default `var_codes`, no `geo`, yes `boundary_year`
+  expect_message(get_geo_estimates(boundary_year="2023"), "requires an `acs_year` parameter", class = "simpleMessage")
+  # no `acs_year`, yes `var_codes`, no `geo`, no `boundary_year`,
+  expect_message(get_geo_estimates(var_codes=c("DP02_0113E","DP02_0115E")), "requires an `acs_year` parameter", class = "simpleMessage")
+  # yes `acs_year`, yes `var_codes`, no `geo`, yes `boundary_year`
+  expect_message(get_geo_estimates(acs_year="2022", var_codes=c("DP02_0113E","DP02_0115E"), boundary_year="2023"), "requires a `geo` parameter", class = "simpleMessage")
 
 })
 
-test_that("incorrect geo and var_codes inputs are handled appropriately", {
+test_that("incorrect inputs are handled appropriately", {
 
-  ### EXPECT MESSAGE
-  # invalid geo
-  expect_message(get_geo_estimates("block party", c("DP02_0113E","DP02_0115E"), "2023"), "This geography could not be found", class = "simpleMessage")
-  # typo in var_codes
-  expect_message(get_geo_estimates("councildist", c("0000","DP02_0115E"), "2023"), "The following variable codes could not be found", class = "simpleMessage")
+  ### EXPECT MESSAGE OR ERROR
+  # invalid `acs_year`
+  expect_message(get_geo_estimates("202", "borough", c("DP02_0113E","DP02_0115E"), "2023"), 'The ACS year "202" could not be found', class = "simpleMessage")
+  # invalid `geo`
+  expect_message(get_geo_estimates("2022", "block party", c("DP02_0113E","DP02_0115E"), "2023"), 'The geography "block party" could not be found', class = "simpleMessage")
+  # typo in `var_codes`
+  expect_error(get_geo_estimates("2022", "councildist", c("0000", "DP02_0115E"), "2023"), "Estimates for the variable code 0000 are not available", class = "simpleError")
+  # invalid `boundary_year`
+  expect_message(get_geo_estimates("2022", "councildist", c("DP02_0113E","DP02_0115E"), "2024"), "Overriding `boundary_year` input to 2023", class = "simpleMessage")
+  # inputs provided without parameter names + default `var_codes`
+  expect_error(get_geo_estimates("2022", "councildist", "2023"), "Estimates for the variable code 2023 are not available", class = "simpleError")
 
 })
 
-test_that("missing, unnecessary, and incorrect boundary_year inputs are handled appropriately", {
+test_that("missing or unnecessary `boundary_year` inputs are handled appropriately", {
 
   ### EXPECT MESSAGE
-  # council w/ no year
-  expect_message(get_geo_estimates("councildist", c("DP02_0113E","DP02_0115E")), "Setting `boundary_year`", class = "simpleMessage")
-  # council w/ incorrect year
-  expect_message(get_geo_estimates("councildist", c("DP02_0113E","DP02_0115E"), "2053"), "`boundary_year` must be '2013' or '2023'", class = "simpleMessage")
-  # non-council with incorrect year
-  expect_message(get_geo_estimates("borough", c("DP02_0113E","DP02_0115E"), "2043"), "`boundary_year` is only relevant for `geo = councildist`", class = "simpleMessage")
-  # non-council with unnecessary year, yes var_codes
-  expect_message(get_geo_estimates("borough", c("DP02_0113E","DP02_0115E"), "2023"), "`boundary_year` is only relevant for `geo = councildist`", class = "simpleMessage")
-  # non-council with unnecessary year, default var_codes
-  expect_message(get_geo_estimates(geo = "borough", boundary_year = "2023"), "`boundary_year` is only relevant for `geo = councildist`", class = "simpleMessage")
+  # councildist w/ no year
+  expect_message(get_geo_estimates("2022", "councildist", c("DP02_0113E","DP02_0115E")), "Overriding `boundary_year` input to 2023", class = "simpleMessage")
+  # non-councildist with incorrect year
+  expect_message(get_geo_estimates("2022", "borough", c("DP02_0113E","DP02_0115E"), "2043"), "Overriding `boundary_year` input to NULL", class = "simpleMessage")
+  # non-councildist with unnecessary (but correct) `boundary_year`, yes `var_codes`
+  expect_message(get_geo_estimates("2022", "borough", c("DP02_0113E","DP02_0115E"), "2023"), "Overriding `boundary_year` input to NULL", class = "simpleMessage")
+  # non-councildist with unnecessary (but correct) `boundary_year`, default var_codes
+  expect_message(get_geo_estimates("2022", geo = "borough", boundary_year = "2023"), "Overriding `boundary_year` input to NULL", class = "simpleMessage")
 
 })
 
 test_that("correct inputs work", {
 
   ### EXPECT NO MESSAGE
-  # yes var_codes, council 2023
-  expect_no_message(get_geo_estimates("councildist", c("DP02_0113E","DP02_0115E"), "2023"), message = "`boundary_year` must be '2013' or '2023'", class = "simpleMessage")
-  # yes var_codes, council 2013
-  expect_no_message(get_geo_estimates("councildist", c("DP02_0113E","DP02_0115E"), "2013"), message = "`boundary_year` must be '2013' or '2023'", class = "simpleMessage")
-  # yes var_codes, non-council w/ no year
-  expect_no_message(get_geo_estimates("borough", c("DP02_0113E","DP02_0115E")), message = "`boundary_year` must be '2013' or '2023'", class = "simpleMessage")
-  # default var_codes, non-council w/ no year
-  expect_no_message(get_geo_estimates("borough"), message = "`boundary_year` must be '2013' or '2023'", class = "simpleMessage")
-  # "all" input for var_codes, non-council w/ no year
-  expect_no_message(get_geo_estimates("borough", "all"), message = "`boundary_year` must be '2013' or '2023'", class = "simpleMessage")
-  # geo and boundary_year provided with parameter names, default var_code
-  expect_no_message(get_geo_estimates(geo = "councildist", boundary_year = "2023"), message = "boundary_year input used for var_codes parameter", class = "simpleMessage")
+  # 'councildist' chosen, `boundary_year` included
+  expect_no_message(get_geo_estimates("2022" ,"councildist", c("DP02_0113E","DP02_0115E"), "2023"), message = "`boundary_year` must be '2013' or '2023'", class = "simpleMessage")
+  # non-councildist w/ no `boundary_year`
+  expect_no_message(get_geo_estimates("2022", "borough", c("DP02_0113E","DP02_0115E")), message = "`boundary_year` must be '2013' or '2023'", class = "simpleMessage")
+  # default `var_codes`, non-councildist w/ no `boundary_year`
+  expect_no_message(get_geo_estimates("2022", "borough"), message = "`boundary_year` must be '2013' or '2023'", class = "simpleMessage")
+  # "all" input for `var_codes`, non-councildist w/ no year
+  expect_no_message(get_geo_estimates("2022", "borough", "all"), message = "`boundary_year` must be '2013' or '2023'", class = "simpleMessage")
+  # `acs_year`, `geo`, and `boundary_year` provided with parameter names, default `var_codes`
+  expect_no_message(get_geo_estimates(acs_year="2022", geo="councildist", boundary_year="2023"), message = "boundary_year input used for var_codes parameter", class = "simpleMessage")
 
 })
 
